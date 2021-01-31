92af1c3c1da2a3f271acdfba455aca990acc876d
Kostadin <seimenski@icloud.com>
2021-01-31 16:48:48 +0200

    Create Accessory Page and Service

---
+++ controllers/accessoryController.js
@@ -0,0 +1,21 @@
+const { Router } = require("express");
+
+const accessoryService = require('../services/accessoryService');
+
+const router = Router();
+
+router.get('/create', (req, res) => {
+    res.render('createAccessory', { title: 'Create Accessory Page' });
+});
+
+router.post('/create', (req, res) => {
+    accessoryService.create(req.body)
+        .then(() => {
+            res.redirect('/products');
+        })
+        .catch((err) => console.log(err));
+
+    
+});
+
+module.exports = router;
--- controllers/productController.js
+++ controllers/productController.js
@@ -5,8 +5,12 @@ const validateProduct = require("../middlewares/validateInput");
 const router = Router();
 
 router.get("/", (req, res) => {
-  let products = productService.getAll(req.query);
-  res.render("home", { title: "Browse", products });
+  productService
+    .getAll(req.query)
+    .then((products) => {
+      res.render("home", { title: "Browse", products });
+    })
+    .catch(() => res.status(500).end());
 });
 
 router.get("/create", (req, res) => {
@@ -20,8 +24,8 @@ router.post("/create", validateProduct, (req, res) => {
     .catch((err) => res.status(500).end());
 });
 
-router.get("/details/:productId", (req, res) => {
-  let product = productService.getOne(req.params.productId);
+router.get("/details/:productId", async (req, res) => {
+  let product = await productService.getOne(req.params.productId)
   res.render("details", { title: "Product Details", product });
 });
 
