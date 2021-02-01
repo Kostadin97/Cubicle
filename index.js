83c87d7ddbe1a62f1e904990d84cfde37f5034c5
Kostadin <seimenski@icloud.com>
2021-02-01 17:18:11 +0200

    init

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 require('./config/express')(app);
 require('./config/mongoose')(app);
 
 app.use(routes);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
+app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}...`));
