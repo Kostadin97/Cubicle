076ec1a0dab6951ab22d6f85ecb372f61a14f1a8
Kostadin <seimenski@icloud.com>
2021-02-13 23:45:45 +0200

    initial commit

--- routes.js
+++ routes.js
@@ -1,8 +1,5 @@
 const { Router } = require("express");
 
-const isAuthenticated = require('./middlewares/isAuthenticated');
-const isGuest = require('./middlewares/isGuest');
-
 const productController = require("./controllers/productController");
 const accessoryController = require("./controllers/accessoryController");
 const authController = require("./controllers/authController");
