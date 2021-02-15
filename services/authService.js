39469ae069ef008db0aacbf43cbf54701f0cafbf
Kostadin <seimenski@icloud.com>
2021-02-15 08:12:09 +0200

    initial commit

--- services/authService.js
+++ services/authService.js
@@ -10,7 +10,7 @@ const register = async ({ username, password }) => {
   let hash = await bcrypt.hash(password, salt);
 
   const user = new User({ username, password: hash });
-  return await user.save();
+  return await user.save();x
 };
 
 const login = async ({ username, password }) => {
